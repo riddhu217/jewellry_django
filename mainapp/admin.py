070554ef4d83970d450d525f569c7db555a708b7
Riddhi <123ridhz@gmail.com>
2020-09-11 14:21:07 +0530

    he

--- mainapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from mainapp.models import Contact
-
-
- #Register your models here.
-
-admin.site.register(Contact)
